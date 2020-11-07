eff69d4822cb25b63b738a9d0559cab696c1e49a
pandenok <dimitri.shatunin@gmail.com>
2020-11-07 16:37:37 +0100

    Pull the flights matching search criteria from database

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,8 @@ class FlightsController < ApplicationController
   include FlightsHelper
   def index
     @flights = Flight.all
+    selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+    @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: selected_date.all_day)
   end
 
   def new
