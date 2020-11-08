06e1079a76169f61becdbe9a0eb89eb23b1b9e9b
pandenok <dimitri.shatunin@gmail.com>
2020-11-08 18:26:07 +0100

    Make a booking form functional through #create action and nested attributes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
+  include BookingsHelper
+
   def new
     @booking = Booking.new
     @selected_flight = Flight.find(params[:flight])
@@ -7,5 +9,16 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash[:success] = 'Booking saved!'
+      redirect_to booking_path(@booking)
+    else
+      flash[:alert] = 'An error occured!'
+      render "new"
+    end
+  end
+
+  def show
   end
 end
--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,5 @@
 module BookingsHelper
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+  end
 end
