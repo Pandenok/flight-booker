eb18c53a88428d5c8b079d29e9008fffa570a097
pandenok <dimitri.shatunin@gmail.com>
2020-11-08 16:20:17 +0100

    Create a form for a new booking with sub-forms for passeggers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @selected_flight = Flight.find(params[:flight])
+    @passengers_count = params[:pax].to_i
+    @passengers_count.times { @booking.passengers.build }
+  end
+
+  def create
+  end
 end
