581bb81ffdbe1b9b0de33c1b0cf2e4bc515e2819
pandenok <dimitri.shatunin@gmail.com>
2020-11-09 14:52:53 +0100

    Remove unused Airport controller and methods in Flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,20 +1,10 @@
 class FlightsController < ApplicationController
   include FlightsHelper
+  
   def index
     if params.has_key?(:day) && params.has_key?(:month) && params.has_key?(:year)
       selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
       @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: selected_date.all_day)
     end 
-  end
-
-  def new
-    @flight = Flight.new
-  end
-
-  def create
-    @flight = Flight.new(flight_params)
-    @flight.save
-    flash[:success] = "Flight was successfully created!"
-    redirect_to root_path
   end
 end
--- app/helpers/airports_helper.rb
+++
@@ -1,5 +0,0 @@
-module AirportsHelper
-  def airport_params
-    params.require(:airport).permit(:code)
-  end
-end
