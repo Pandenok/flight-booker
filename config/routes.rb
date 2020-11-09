686849cc35abf634ea978f31aa0da3b1c7d3cff6
pandenok <dimitri.shatunin@gmail.com>
2020-11-09 15:17:21 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'flights#index'
 
-  resources :airports
-  resources :flights
-  resources :bookings
+  resources :flights, only: [:index]
+  resources :bookings, only: [:new, :create, :show]
 end
