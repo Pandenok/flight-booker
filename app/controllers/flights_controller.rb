7a65c87a6ca4d81c45add1e907a1969e68f3e2a0
pandenok <dimitri.shatunin@gmail.com>
2020-11-09 12:41:39 +0100

    Fix bug in Flights controller on invalid date upon loading

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,10 @@
 class FlightsController < ApplicationController
   include FlightsHelper
   def index
-    @flights = Flight.all
-    selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-    @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: selected_date.all_day)
+    if params.has_key?(:day) && params.has_key?(:month) && params.has_key?(:year)
+      selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+      @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: selected_date.all_day)
+    end 
   end
 
   def new
