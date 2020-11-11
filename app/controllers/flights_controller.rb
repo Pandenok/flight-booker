a4ca6a0c4e39fa1213de2b4fe261c702ccf80fc1
pandenok <dimitri.shatunin@gmail.com>
2020-11-11 21:12:24 +0100

    CSS styling with bulma gem

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,16 +4,16 @@ class FlightsController < ApplicationController
   def index
     if params.has_key?(:day) && params.has_key?(:month) && params.has_key?(:year)
       if params[:from_airport_id].blank?
-        flash.now[:alert] = "please choose departure airport"
+        flash.now[:alert] = "Departure airport is missing"
       elsif params[:to_airport_id].blank?
-        flash.now[:alert] = "please choose arrival airport"
+        flash.now[:alert] = "Destination airport is missing"
       elsif params[:pax].blank?
-        flash.now[:alert] = "please choose the number of passengers"
+        flash.now[:alert] = "Please choose the number of passengers"
       elsif params[:from_airport_id] == params[:to_airport_id]
-        flash.now[:alert] = "Please verify your choice"
+        flash.now[:alert] = "Please check your input"
       else
-        selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-        @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: selected_date.all_day)
+        @selected_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+        @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: @selected_date.all_day)
       end
     end 
   end
