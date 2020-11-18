f7ddabf2ae2cb4cfefc3c742af2cdaaaa0586811
pandenok <dimitri.shatunin@gmail.com>
2020-11-18 17:19:52 +0100

    Create the action to send the confirmation email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'confirmations@example.com'
+
+  def confirmation_email(passenger)
+    @passenger = passenger
+    
+    mail(to: @passenger.email, subject: 'Confirmation FlightBooker')
+  end
 end
